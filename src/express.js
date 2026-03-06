fc897f564993b528befcb47cb93b2359acb200c7
Johan Kronholm <kronholmjohan@gmail.com>
2026-03-06 17:26:18 +0100

    Refactor session middleware configuration

--- src/express.js
+++ src/express.js
@@ -2,6 +2,7 @@ import express, { urlencoded } from "express";
 import helmet from "helmet";
 import { router } from "./routes/index.js";
 import session from "express-session";
+import { sessOptions } from "./config/session.js";
 
 export const app = express();
 
@@ -10,17 +11,7 @@ app.use(helmet());
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(
-  session({
-    secret: process.env.PRIVATE_KEY,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      httpOnly: true,
-      secure: false,
-    },
-  }),
-);
+app.use(session(sessOptions));
 
 app.set("view engine", "ejs");
 app.set("views", "src/views");
